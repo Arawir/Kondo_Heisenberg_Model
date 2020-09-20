0d0613bd701298b20b9ac1d41391ff048f0cdcfb
Arawir <mmarciniak87@wp.pl>
2020-09-20 13:17:54 +0200

    wip

--- interface.h
+++ interface.h
@@ -12,9 +12,10 @@
 #include <string>
 #include <array>
 
+using namespace itensor;
+
 typedef std::complex<double> cpx;
 #define im std::complex<double>{0.0,1.0}
-
 
 inline void massert(std::string info)
 {
@@ -22,28 +23,76 @@ inline void massert(std::string info)
     assert(false);
 }
 
-using namespace itensor;
-
-double getD(std::string data)
+inline double getD(std::string data)
 {
     return Args::global().getReal(data);
 }
-
-int getI(std::string data)
+inline int getI(std::string data)
 {
     return Args::global().getInt(data);
 }
-
-bool getB(std::string data)
+inline bool getB(std::string data)
 {
     return Args::global().getBool(data);
 }
-
-std::string getS(std::string data)
+inline std::string getS(std::string data)
 {
     return Args::global().getString(data);
 }
 
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+
+template <class T>
+class BasicObjectList{
+protected:
+    std::vector<T*> objects;
+
+public:
+    BasicObjectList() : objects{} { }
+    ~BasicObjectList()
+    {
+        for(auto& object : objects){
+            delete object;
+        }
+    }
+
+    T& operator () (std::string name)
+    {
+        return *get(name);
+    }
+
+protected:
+    T* get(std::string name)
+    {
+        for(auto &object : objects){
+            if(object->name == name) return object;
+        }
+        massert("ERROR: Wrong object name ("+name+")!");
+        return nullptr;
+    }
+    bool exists(std::string name)
+    {
+        for(auto &object : objects){
+            if(object->name == name) return true;
+        }
+        return false;
+    }
+
+
+};
+/////////////////////////////
 struct Param{
     std::string name;
     std::string type;
@@ -72,16 +121,13 @@ struct Param{
     }
 };
 
-class Parameters{
-private:
-    std::vector<Param> params;
-
+class Parameters : public BasicObjectList<Param>{
 public:
     Parameters(){ }
     ~Parameters() = default;
 
     void add(std::string name, std::string type, std::string value){
-        params.push_back( Param{name, type, value} );
+        objects.push_back( new Param{name, type, value} );
     }
 
     void set(int argc, char *argv[]){
@@ -93,21 +139,21 @@ public:
     }
 
     bool getBool(std::string name){
-        return param(name)->getBool();
+        return get(name)->getBool();
     }
     int getInt(std::string name){
-        return param(name)->getInt();
+        return get(name)->getInt();
     }
     double getDouble(std::string name){
-        return param(name)->getDouble();
+        return get(name)->getDouble();
     }
 
     void write(){
         std::cout << "---------------------------- System parameters ---------------------------" << std::endl;
-        for(auto& param : params){
-            std::cout << "  " << param.name << " -> ";
-            std::cout << "  " << param.value << " ";
-            if(!param.wasSet){ std::cout << "(default)"; }
+        for(auto& param : objects){
+            std::cout << "  " << param->name << " -> ";
+            std::cout << "  " << param->value << " ";
+            if(!param->wasSet){ std::cout << "(default)"; }
             std::cout << std::endl;
         }
     }
@@ -115,34 +161,25 @@ public:
 private:
     void set(std::string data){
         size_t pos = data.find("=");
-        param( data.substr(0,pos) )->value = data.substr(pos+1);
-        param( data.substr(0,pos) )->wasSet = true;
+        get( data.substr(0,pos) )->value = data.substr(pos+1);
+        get( data.substr(0,pos) )->wasSet = true;
     }
 
     void addArgs(){
-        for(auto &param : params){
-            if(param.type=="bool"){
-                Args::global().add(param.name, param.getBool());
-            } else if(param.type=="string"){
-                Args::global().add(param.name, param.value);
-            } else if(param.type=="int"){
-                Args::global().add(param.name, param.getInt());
-            } else if(param.type=="double"){
-                Args::global().add(param.name, param.getDouble());
+        for(auto &param : objects){
+            if(param->type=="bool"){
+                Args::global().add(param->name, param->getBool());
+            } else if(param->type=="string"){
+                Args::global().add(param->name, param->value);
+            } else if(param->type=="int"){
+                Args::global().add(param->name, param->getInt());
+            } else if(param->type=="double"){
+                Args::global().add(param->name, param->getDouble());
             }
         }
     }
-
-
-    Param* param(std::string name){
-        for(auto &param : params){
-            if(param.name == name) return &param;
-        }
-        massert("ERROR: Wrong parameter name (" +name + ")!");
-        return nullptr;
-    }
 } Params;
-
+//////////////////////////////
 
 std::vector<std::string> separateSubBlocs(std::string str)
 {
@@ -192,7 +229,6 @@ std::vector<std::string> expandSubBlocs(std::vector<std::string> subBlocs)
     }
     return out;
 }
-
 std::vector<std::string> parseInitState(std::string str)
 {
     int L = Params.getInt("L");
@@ -212,9 +248,6 @@ std::vector<std::string> parseInitState(std::string str)
     }
     return state;
 }
-
-
-
 itensor::MPS prepareInitState(auto &sites)
 {
     if( (Args::global().getString("state")=="random")||(Args::global().getString("state")=="r")){
@@ -228,8 +261,6 @@ itensor::MPS prepareInitState(auto &sites)
     std::vector<std::string> initState = parseInitState(Args::global().getString("state"));
     int L = Params.getInt("L");
 
-
-
     auto state = itensor::InitState(sites);
     for(int i=1;i<=L;i++){
         state.set(i,initState[i-1]);
@@ -240,39 +271,7 @@ itensor::MPS prepareInitState(auto &sites)
     return itensor::MPS(state);
 }
 
-struct Experiment
-{
-    std::string name;
-    std::function<void(void)> experiment;
-
-    Experiment(std::string nName) :
-        name{nName}
-    {
-
-    }
-
-    void operator = (std::function<void(void)> exp)
-    {
-        experiment = exp;
-    }
-};
-
-
-std::string exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
-}
-
 ///////////////////////////////////////////
-
 struct Observable
 {
     std::string name;
@@ -326,10 +325,9 @@ enum class oMode{
     a, b, c
 };
 
-class ObservableContainer
+class ObservableContainer : public BasicObjectList<Observable>
 {
-public:
-    std::vector<Observable> observables;
+protected:
     oMode mode = oMode::a;
     clock_t  time0, timeLast;
 
@@ -339,15 +337,6 @@ public:
         , timeLast{ clock() }
     { }
     ~ObservableContainer() = default;
-
-    Observable& operator () (std::string name)
-    {
-        for(auto& obs : observables){
-            if(obs.name == name){ return obs; }
-        }
-        observables.push_back( Observable{name} );
-        return observables.back();
-    }
 
     template<typename T,typename... Args>
     void calc(MPS &psi, T val, Args... args)
@@ -372,42 +361,26 @@ public:
 
     void setSites(KH &sites)
     {
-        for(auto& obs : observables){
-            obs.sites = &sites;
+        for(auto& obs : objects){
+            obs->sites = &sites;
         }
     }
 
 
 private:
-    Observable* observable(std::string name)
-    {
-        for(auto& obs : observables){
-            if(obs.name == name){ return &obs; }
-        }
-        massert("ERROR: unknown observable name!");
-        return nullptr;
-    }
-    bool exists(std::string name)
-    {
-        for(auto& obs : observables){
-            if(obs.name == name){ return true; }
-        }
-        return false;
-    }
-
     void initObservablesIfNeeded()
     {
-        for(auto& obs : observables){
-            obs.generateIfNeeded();
+        for(auto& obs : objects){
+            obs->generateIfNeeded();
         }
     }
 
     void writeObservableValue(std::string name, MPS &psi)
     {
         if( exists(name) == true){
-            observable(name)->generateIfNeeded();
+            get(name)->generateIfNeeded();
             if(mode==oMode::b || mode==oMode::c){ std::cout << name << "= "; }
-            for(auto val : observable(name)->expectedValue(psi)){
+            for(auto val : get(name)->expectedValue(psi)){
                 std::cout << val.real() << " ";
             }
         } else if(name=="dim"){
@@ -433,23 +406,11 @@ private:
 /////////////////////////////////////////////////
 class Controller : public ObservableContainer
 {
-
 public:
-
-    Controller() :
-        ObservableContainer{ }
-    {
-
-    }
+    Controller() : ObservableContainer{ } { }
     ~Controller() = default;
 
     void addPoint(std::string text)
-    {
-        writePointInfo(text);
-    }
-
-private:
-    void writePointInfo(std::string text)
     {
         std::cout << "--------------------";
         std::cout << text;
@@ -465,41 +426,30 @@ private:
 } ExpCon;
 
 ////////////////////////////////////////////////
-class ExperimentsClass
+struct Experiment
 {
-private:
-    std::vector<Experiment> experiments;
+    std::string name;
+    std::function<void(void)> experiment;
+
+    Experiment(std::string nName) : name{nName} { }
+
+    void operator = (std::function<void(void)> exp)
+    {
+        experiment = exp;
+    }
+};
+
+class ExperimentsClass : public BasicObjectList<Experiment>
+{
 public:
-    ExperimentsClass() :
-        experiments {  }
-    {
-
-    }
+    ExperimentsClass() { }
     ~ExperimentsClass() = default;
-
-    Experiment& operator () (std::string name)
-    {
-        for(auto& exp : experiments){
-            if(exp.name == name){ return exp; }
-        }
-        experiments.push_back( Experiment{name} );
-        return experiments.back();
-    }
 
     void run()
     {
         ExpCon.addPoint("Start");
-        experiment(getS("exp"))->experiment();
+        get(getS("exp"))->experiment();
         ExpCon.addPoint("Finish");
-    }
-
-    Experiment* experiment(std::string name)
-    {
-        for(auto& exp : experiments){
-            if(exp.name == name){ return &exp; }
-        }
-        massert("ERROR: unknown experiment name!");
-        return nullptr;
     }
 
 } Experiments;
