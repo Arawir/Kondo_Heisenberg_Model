e39eede66328da34995c083f2574407ad6a914e2
Arawir <mmarciniak87@wp.pl>
2020-09-21 11:45:08 +0200

    wip

--- interface.h
+++ interface.h
@@ -14,15 +14,19 @@
 
 using namespace itensor;
 
+//////SET THIS////////////////////
+using SiteSetType=KH;
+//////////////////////////////////
+
 typedef std::complex<double> cpx;
 #define im std::complex<double>{0.0,1.0}
+
 
 inline void massert(std::string info)
 {
     std::cerr << info << std::endl;
     assert(false);
 }
-
 inline double getD(std::string data)
 {
     return Args::global().getReal(data);
@@ -50,7 +54,8 @@ std::string exec(const char* cmd) {
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
         result += buffer.data();
     }
-    return result;
+    auto pos = result.find("\n");
+    return result.substr(0,pos-1);
 }
 
 
@@ -70,7 +75,9 @@ public:
 
     T& operator () (std::string name)
     {
-        return *get(name);
+        if( exists(name) ){ return *get(name); }
+        objects.push_back( new T{name} );
+        return *objects.back();
     }
 
 protected:
@@ -196,33 +203,30 @@ std::vector<std::string> separateSubBlocs(std::string str)
     }
     return out;
 }
-
-int identifyMultiplayer(std::string data)
+int identifyMultiplier(std::string data)
 {
     if(data.find("L/")!=std::string::npos){
         return getI("L")/atoi( data.substr(data.find("L/")+2).c_str() );
     }
     return atoi(data.c_str());
 }
-
-std::tuple<int,std::string> separateMultiplayer(std::string data)
+std::tuple<int,std::string> separateMultiplier(std::string data)
 {
-    int multiplayer=1;
+    int multiplier=1;
     std::string state=data;
     if(data.find("*")!=std::string::npos){
-        multiplayer = identifyMultiplayer( data.substr(0,data.find("*")) );
+        multiplier = identifyMultiplier( data.substr(0,data.find("*")) );
         state = data.substr(data.find("*")+1);
     }
 
-    return std::make_tuple( multiplayer,state );
+    return std::make_tuple( multiplier,state );
 }
-
 std::vector<std::string> expandSubBlocs(std::vector<std::string> subBlocs)
 {
     std::vector<std::string> out{};
 
     for(auto subBlock : subBlocs){
-        auto [n,state] = separateMultiplayer(subBlock);
+        auto [n,state] = separateMultiplier(subBlock);
         for(int i=0; i<n; i++){
             out.push_back(state);
         }
@@ -278,16 +282,16 @@ struct Observable
     std::vector<MPO> matrix;
     bool wasGenerated=false;
     bool useMultMpos = false;
-    std::function<MPO(const KH &sites)> generateMPO;
-    std::function<std::vector<MPO>(const KH &sites)> generateMPOs;
-    KH *sites = nullptr;
+    std::function<MPO(const SiteSetType &sites)> generateMPO;
+    std::function<std::vector<MPO>(const SiteSetType &sites)> generateMPOs;
+    SiteSetType *sites = nullptr;
 
-    void operator = (std::function<MPO(const KH &sites)> generate)
+    void operator = (std::function<MPO(const SiteSetType &sites)> generate)
     {
         generateMPO = generate;
     }
 
-    void operator = (std::function<std::vector<MPO>(const KH &sites)> generate)
+    void operator = (std::function<std::vector<MPO>(const SiteSetType &sites)> generate)
     {
         useMultMpos=true;
         generateMPOs = generate;
@@ -359,7 +363,7 @@ public:
         std::cout << std::endl;
     }
 
-    void setSites(KH &sites)
+    void setSites(SiteSetType &sites)
     {
         for(auto& obs : objects){
             obs->sites = &sites;
